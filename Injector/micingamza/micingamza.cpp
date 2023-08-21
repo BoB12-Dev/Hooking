4b5ed6c1e1f704f2877bac50115048719f576754
2W <psh0036@suwon.ac.kr>
2023-08-21 23:20:36 +0900

    Add Target

---
+++ Injector/micingamza/micingamza.cpp
@@ -0,0 +1,25 @@
+﻿// micingamza.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+
+int ScanMem(char* szSrc, int nSrcSize, char* szTarget, int nTargetSize)
+{
+    for (int i = 0; i < nSrcSize; i++) {
+        if (0 == memcmp(szSrc + i, szTarget, nTargetSize))
+        {
+            printf("찾았다! : %i\n" , i);
+            return true;
+        }
+    }
+    return false;
+}
+
+
+int main()
+{
+    char* p = (char*)malloc(4096 * 10);
+
+    ScanMem(p,4096*10,(char*)"micingamza",10);
+}
